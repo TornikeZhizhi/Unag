dfd11cb15c51f1dd9200e2f2c61881bb295d2878
tornike <zhizhiashvilitornike@gmail.com>
2021-05-14 02:37:54 +0400

    add home page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+$('.rate_slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots:false,
+  arrows:true,
+   prevArrow: $('.slick_left_arrow'),
+     nextArrow: $('.slick_right_arrow')
+
+
+
+});
+
+})
